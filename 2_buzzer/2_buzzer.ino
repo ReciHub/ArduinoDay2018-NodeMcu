f90b27058412a73d8e6a6be857a752879f6687fe
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-04-07 01:46:42 -0300

    Added buzzer song file

---
+++ 2_buzzer/2_buzzer.ino
@@ -0,0 +1,30 @@
+#include "pitches.h"
+
+// Notas na melodia
+int melody[] = {
+  NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_F4, NOTE_F4
+};
+
+int noteDurations[] = {
+  8, 8, 8, 8, 4, 4
+};
+
+void setup() {
+  // Itera sobre os valores diferentes das notas
+  for (int thisNote = 0; thisNote < 6; thisNote++) {
+
+    // Calcula o tempo de duracao da nota
+    int noteDuration = 100 * noteDurations[thisNote];
+    tone(14, melody[thisNote], noteDuration);
+
+    // Para entenderemos qual a nota que esta tocando, eh interessante dar uma pausa entre elas
+    int pauseBetweenNotes = noteDuration * 0.4;
+    delay(pauseBetweenNotes);
+    // para o tone que esta tocando no momento
+    noTone(14);
+  }
+}
+
+void loop() {
+  // nao repete nada
+}
