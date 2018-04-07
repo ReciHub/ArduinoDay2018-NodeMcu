c4172029fef7270e05dbe3a3de5ac30a8387e962
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-04-07 13:03:33 -0300

    Added Latest files

--- 3_msg_servidor/3_msg_servidor.ino
+++ 3_msg_servidor/3_msg_servidor.ino
@@ -1,68 +1,72 @@
-	
+
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-char nome[] = "";
-char read;
-int index = 0;
-char frase[] = "";
+char nome[30] = "THiago";
+int i = 0;
+char frase[50] = "Oi, sou jose";
+char inChar;
 
+char mensagem[50]  = {};
 void setup() {
- 
-  Serial.begin(115200);                                  //Serial connection
-  WiFi.begin("yourSSID", "yourPASS");   //WiFi connection
- 
-  Serial.println("Qual o seu nome?");
 
-  while(Serial.available() > 0){
-      inChar = Serial.read(); // Read a character
-          inData[index] = inChar; // Store it
-          index++; // Increment where to write next
-  }
-
+  Serial.begin(9600);                                  //Serial connection
+  
+  WiFi.begin("CINGUESTS", "acessocin");   //WiFi connection
   while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
- 
     delay(500);
     Serial.println("Waiting for connection");
- 
   }
- 
-}
- 
-void loop() {
- 
- if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
-   
-   Serial.println("Digite uma frase para o chat");
-
-    while(Serial.available() > 0){
-      inChar = Serial.read(); // Read a character
-      if(inChar =`'\n')
+  
+  Serial.println("Qual o seu nome?");
+  while (Serial.available() > 0) {
+    inChar = Serial.read(); // Read a character
+    nome[i] = inChar; // Store it
+    i++; // Increment where to write next
+    if(inChar == '\n')
       break;
-          frase[index] = inChar; // Store it
-          index++; // Increment where to write next
-     }
+    Serial.print("I received: ");
+    Serial.println(inChar, DEC);
+  }
+
+  
+
+}
+
+void loop() {
+
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+
+    Serial.println("Digite uma frase para o chat");
+    i = 0;
+    while (Serial.available() > 0) {
+      inChar = Serial.read(); // Read a character
+      if (inChar = '\n')
+        break;
+      frase[i] = inChar; // Store it
+      i++; // Increment where to write next
+    }
 
 
-   HTTPClient http;    //Declare object of class HTTPClient
- 
-   http.begin("http://192.168.1.88:8085/hello");      //Specify request destination
-   http.addHeader("Content-Type", "text/plain");  //Specify content-type header
- 
-   int httpCode = http.POST(ñome + ": "+ frase);   //Send the request
-   String payload = http.getString();                  //Get the response payload
- 
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection
- 
- }else{
- 
-    Serial.println("Error in WiFi connection");   
- 
- }
- 
-  delay(30000);  //Send a request every 30 seconds
- 
+    HTTPClient http;    //Declare object of class HTTPClient
+
+    http.begin("http://dev-thiagoaugustomartins.c9users.io:8080/mensagem");      //Specify request destination
+    http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+    String mensagem = String(nome) + String(": ") + String(frase);
+    int httpCode = http.POST(mensagem);  //Send the request
+    String payload = http.getString();                  //Get the response payload
+
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+
+    http.end();  //Close connection
+
+  } else {
+
+    Serial.println("Error in WiFi connection");
+
+  }
+
+  delay(1000);  //Send a request every 30 seconds
+
 }
